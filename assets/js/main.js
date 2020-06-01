2824d65cf5b715988b6bd6662221f48c0f3853ab
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-05-31 22:06:53 -0700

    Finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,27 @@ window.onload = function() {
     Particles.init({
         selector: '.background',
         color: ['#f2a900', '#808080', '#DBEDF3'],
-        connectParticles: true
+        connectParticles: true,
+        sizeVariations: 5,
+        maxParticles: 250
     });
 };
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+})
 
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
