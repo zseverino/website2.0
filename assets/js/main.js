7f40147774fa1f99c17393535212d513bbf2c41f
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-05-31 19:07:17 -0700

    Fixed Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,40 @@ window.onload = function() {
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
-    var tags = $("section")
+    var tags = $("h1")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+})
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("h2")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+})
+
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("h3")
 
     for (var i = 0; i < tags.length; i++) {
         var tag = tags[i]
