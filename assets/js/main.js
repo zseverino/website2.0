f427aa14ddedad911176a6a159c9f194f8c20309
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-05-31 18:17:50 -0700

    Original Upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        color: ['#f2a900', '#808080', '#DBEDF3'],
+        connectParticles: true
+    });
+};
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+})
